996e500b21be44d79ff7ae3535da4a8d9aadd9c6
Craig Miller <craigmiller160@gmail.com>
2019-11-03 19:18:59 -0500

    Added polyfill code

---
+++ gulp/jsLib.gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const clean = require('gulp-clean');
+const path = require('path');
+
+gulp.task('compile', () => {
+	const cwd = process.cwd();
+	const babelConfig = require(`${cwd}/babel.config.js`);
+	return gulp.src([
+			path.resolve(cwd, './src/**/*.js'),
+			path.resolve(cwd, './src/**/*.jsx')
+		])
+		.pipe(babel(babelConfig))
+		.pipe(gulp.dest(path.resolve(cwd, 'lib')));
+});
+
+gulp.task('clean', () => {
+	const cwd = process.cwd();
+	return gulp.src(path.resolve(cwd, 'lib'), { allowEmpty: true })
+		.pipe(clean());
+});
+
+gulp.task('buildLib', gulp.series('clean', 'compile'));
