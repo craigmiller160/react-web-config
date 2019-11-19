14db94a2a11fa6a754a098510af236781f523cbd
Craig Miller <craigmiller160@gmail.com>
2019-11-19 09:04:41 -0500

    Added hash config to resources webpack config

--- webpack/webpack.resources.js
+++ webpack/webpack.resources.js
@@ -1,51 +1,55 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const createConfig = ({ copyPaths = [] } = {}) => ({
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif|bmp|svg)$/,
-                include: /images[\\\/].*\.(png|jpe?g|gif|bmp|svg)$/,
-                exclude: /fonts/,
-                use: [
-                    {
+const createConfig = ({ copyPaths = [], useFilenameHash = true } = {}) => {
+    const hash = useFilenameHash ? '[hash:8].' : '';
+
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpe?g|gif|bmp|svg)$/,
+                    include: /images[\\\/].*\.(png|jpe?g|gif|bmp|svg)$/,
+                    exclude: /fonts/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 30000,
+                                name: `[name].${hash}[ext]`,
+                                outputPath: 'assets/images',
+                                publicPath: '../../assets/images'
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.ico/,
+                    use: {
                         loader: 'url-loader',
                         options: {
-                            limit: 30000,
-                            name: '[name].[hash:8].[ext]',
-                            outputPath: 'assets/images',
-                            publicPath: '../../assets/images'
+                            name: '[name].[ext]'
                         }
                     }
-                ]
-            },
-            {
-                test: /\.ico/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        name: '[name].[ext]'
+                },
+                {
+                    test: /\.(eot|otf|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+                    include: /fonts[\\\/].*\.(eot|otf|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+                    exclude: /images/,
+                    use: {
+                        loader: 'file-loader',
+                        options: {
+                            name: `[name].${hash}[ext]`,
+                            outputPath: 'assets/fonts/',
+                            publicPath: '../../assets/fonts'
+                        }
                     }
                 }
-            },
-            {
-                test: /\.(eot|otf|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
-                include: /fonts[\\\/].*\.(eot|otf|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
-                exclude: /images/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[hash:8].[ext]',
-                        outputPath: 'assets/fonts/',
-                        publicPath: '../../assets/fonts'
-                    }
-                }
-            }
+            ]
+        },
+        plugins: [
+            new CopyWebpackPlugin(copyPaths)
         ]
-    },
-    plugins: [
-        new CopyWebpackPlugin(copyPaths)
-    ]
-});
+    };
+};
 
 module.exports = createConfig;
