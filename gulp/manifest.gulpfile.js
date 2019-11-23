4d10656e97fd6b3254e669b02efc7fab90154678
Craig Miller <craigmiller160@gmail.com>
2019-11-23 13:39:03 -0500

    Cleaned up file paths in manifest

--- gulp/manifest.gulpfile.js
+++ gulp/manifest.gulpfile.js
@@ -2,7 +2,12 @@ const gulp = require('gulp');
 const path = require('path');
 const fs = require('fs');
 
+// TODO need to filter out LICENSE and html files
+// TODO need to remove cwd and build from file paths
+
 const cwd = process.cwd();
+
+console.log('CWD', cwd); // TODO delete this
 
 const walk = (dir) => {
     let results = [];
@@ -19,17 +24,22 @@ const walk = (dir) => {
     return results;
 };
 
+const cleanFile = (file) =>
+    file.replace(cwd, '')
+        .replace('/build', '');
+
 const createManifest = () => {
     const files = walk(path.resolve(cwd, 'build'));
 
     const output = files.reduce((acc, file) => {
-        if (/\.js$/.test(file)) {
+        if (/\.js$/.test(cleanFile(file))) {
             acc.js.push(file);
         } else if (/\.css$/.test(file)) {
-            acc.css.push(file);
+            acc.css.push(cleanFile(file));
         } else {
-            acc.other.push(file);
+            acc.other.push(cleanFile(file));
         }
+        return acc;
     }, { js: [], css: [], other: [] });
 
     fs.writeFileSync(path.resolve(cwd, 'build/file-manifest.json'), JSON.stringify(output));
