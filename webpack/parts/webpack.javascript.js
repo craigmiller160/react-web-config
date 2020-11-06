ffbf67b2e1c2e9107e6679e19898a6b0d39f5682
Craig <craigmiller160@gmail.com>
2020-11-06 14:49:14 -0500

    Improved ability to use custom babel config

--- webpack/parts/webpack.javascript.js
+++ webpack/parts/webpack.javascript.js
@@ -1,5 +1,13 @@
 const path = require('path');
-const babelConfig = require(path.resolve(process.cwd(), 'babel.config.js'));
+const fs = require('fs');
+
+let babelConfig;
+const customBabelConfigPath = path.resolve(process.cwd(), 'babel.config.js');
+if (fs.existsSync(customBabelConfigPath)) {
+    babelConfig = require(customBabelConfigPath);
+} else {
+    babelConfig = require(path.resolve(__dirname, '../../babel'));
+}
 
 module.exports = {
     resolve: {
