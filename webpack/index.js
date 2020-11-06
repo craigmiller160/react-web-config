5c9013aa15821d2225007fce841ece9260819104
Craig <craigmiller160@gmail.com>
2020-11-06 15:06:51 -0500

    Integrated webpack minify

--- webpack/index.js
+++ webpack/index.js
@@ -2,12 +2,14 @@ const webpackBase = require('./parts/webpack.base');
 const webpackHtml = require('./parts/webpack.html');
 const webpackJs = require('./parts/webpack.javascript');
 const webpackAnalyze = require('./parts/webpack.analyze');
+const webpackMinify = require('./parts/webpack.minify');
 const merge = require('webpack-merge');
 
 const parts = [
     webpackBase,
     webpackHtml,
-    webpackJs
+    webpackJs,
+    webpackMinify
 ];
 
 if (process.env.ANALYZE) {
