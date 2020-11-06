3e3c036db24c71544b5b15355d4c7a8a6fea6a93
Craig <craigmiller160@gmail.com>
2020-11-06 13:59:56 -0500

    Added webpack base

---
+++ webpack/parts/webpack.base.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: path.resolve(process.cwd(), 'src'),
+    resolve: {
+        modules: [
+            path.resolve(process.cwd(), 'src'),
+            path.resolve(process.cwd(), 'node_modules')
+        ]
+    },
+    output: {
+        path: path.resolve(process.cwd(), 'build'),
+        filename: 'assets/js/app.[hash:8].js',
+        chunkFilename: 'assets/js/[name].[hash:8].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+};
