2d041502a12aa8cc764d77f6eca53b9b155bab49
Craig <craigmiller160@gmail.com>
2020-11-06 15:17:35 -0500

    More refactoring

--- webpack/index.js
+++ webpack/index.js
@@ -2,7 +2,7 @@ const webpackBase = require('./parts/webpack.base');
 const webpackHtml = require('./parts/webpack.html');
 const webpackJs = require('./parts/webpack.javascript');
 const webpackAnalyze = require('./parts/webpack.analyze');
-const webpackMinify = require('./parts/webpack.minify');
+const webpackOptimize = require('./parts/webpack.optimize');
 const webpackResources = require('./parts/webpack.resources');
 const merge = require('webpack-merge');
 
@@ -10,7 +10,7 @@ const parts = [
     webpackBase,
     webpackHtml,
     webpackJs,
-    webpackMinify
+    webpackOptimize
 ];
 
 if (process.env.ANALYZE) {
