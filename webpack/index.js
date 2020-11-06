a6cd093b1af7896dc7dd32cdf43ee976a660fa41
Craig <craigmiller160@gmail.com>
2020-11-06 15:31:52 -0500

    Setup webpack css handling

--- webpack/index.js
+++ webpack/index.js
@@ -4,13 +4,15 @@ const webpackJs = require('./parts/webpack.javascript');
 const webpackAnalyze = require('./parts/webpack.analyze');
 const webpackOptimize = require('./parts/webpack.optimize');
 const webpackResources = require('./parts/webpack.resources');
+const webpackCss = require('./parts/webpack.css');
 const merge = require('webpack-merge');
 
 const parts = [
     webpackBase,
     webpackHtml,
     webpackJs,
-    webpackOptimize
+    webpackOptimize,
+    webpackCss
 ];
 
 if (process.env.ANALYZE) {
--- webpack/parts/webpack.css.js
+++ webpack/parts/webpack.css.js
@@ -1,9 +1,41 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const loaders = (isCssModule) => ([
+    process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+    {
+        loader: 'css-loader',
+        options: {
+            importLoaders: 2
+        }
+    },
+    {
+        loader: 'postcss-loader',
+        options: {
+            config: {
+                path: path.resolve(__dirname, 'postcss.config.js')
+            }
+        }
+    },
+    'resolve-url-loader'
+]);
 
 module.exports = {
     plugins: [
-        new MiniCssExtractPlugin({
-
-        })
-    ]
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: loaders(false),
+                sideEffects: true
+            },
+            {
+                test: /\.module\.css$/,
+                use: loaders(true)
+            }
+        ]
+    }
 };
