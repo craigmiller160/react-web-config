d006a4eaf172eb2ebf5e587879969d93095e530e
Craig <craigmiller160@gmail.com>
2020-11-07 21:32:19 -0500

    Fixed jest config paths

--- jest/index.js
+++ jest/index.js
@@ -4,31 +4,27 @@ const { defaults } = require('jest-config');
 module.exports = {
     ...defaults,
     setupFilesAfterEnv: [
-        '<rootDir>/test/setup.ts' // TODO migrate this to this project
+        path.resolve(process.cwd(), 'test', 'setup.ts') // TODO migrate this to this project
     ],
     collectCoverage: true,
     collectCoverageFrom: [
-        '<rootDir>/src/**/*.{ts,tsx}',
-        '!<rootDir>/node_modules/',
-        '!<rootDir>/src/**/*.d.ts'
+        path.resolve(process.cwd(), 'src/**/*.{js,jsx,ts,tsx}'),
+        `!${path.resolve(process.cwd(), 'node_modules')}`,
+        `!${path.resolve(process.cwd(), 'src/**/*.d.ts')}`
     ],
     moduleNameMapper: {
         '\\.(scss)$': 'identity-obj-proxy'
     },
     modulePaths: [
-        '<rootDir>/src'
+        path.resolve(process.cwd(), 'src')
     ],
     testMatch: [
-        '<rootDir>/test/**/*.test.{ts,tsx}'
-    ],
-    moduleDirectories: [
-        'node_modules',
-        'src'
+        path.resolve(process.cwd(), 'test/**/*.test.{js,jsx,ts,tsx}')
     ],
     testEnvironment: 'jsdom',
     testURL: 'http://localhost',
     modulePathIgnorePatterns: [
-        '<rootDir>/.yalc'
+        path.resolve(process.cwd(), '.yalc')
     ],
     preset: 'ts-jest' // TODO try using a transformer instead so you can cover JS & TS files
 };
