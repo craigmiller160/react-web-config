e6d4f22d09b9313192f0c91a20a504c29201d6d9
Craig <craigmiller160@gmail.com>
2020-11-08 15:10:16 -0500

    Working on webpack build script

---
+++ scripts/webpack.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const webpackConfig = require('../webpack');
+
+const webpackCompiler = webpack(webpackConfig);
+
+console.log('Running webpack build');
+
+webpackCompiler.run((err, stats) => {
+    if (err) {
+        console.error(err.stack || err);
+        if (err.details) {
+            console.error(err.details);
+        }
+        return;
+    }
+
+    // const info = stats.toJson();
+    //
+    // if (stats.hasErrors()) {
+    //     console.errors(info.errors);
+    // }
+
+    const output = stats.toString({
+        colors: true
+    });
+
+    console.log(output);
+});
