5b49b7707363269d62a3192f9fd512d7d9e29762
Craig <craigmiller160@gmail.com>
2020-11-08 15:52:08 -0500

    Added NODE_ENV=production to buildTSLib

--- scripts/buildTSLib.js
+++ scripts/buildTSLib.js
@@ -9,7 +9,9 @@ const copyLibResources = require('./utils/copyLibResources');
 const execute = () => {
     fs.rmdirSync(path.resolve(process.cwd(), 'lib'), { recursive: true });
 
-    const result = spawn.sync('tsc', [
+    const result = spawn.sync('cross-env', [
+        'NODE_ENV=production',
+        'tsc',
         '--project',
         getTsConfigPath()
     ], { stdio: 'inherit' });
