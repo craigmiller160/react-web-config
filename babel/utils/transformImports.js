32c9c3b1203c8574bebf04b47e18ca3214b54de4
Craig Miller <craigmiller160@gmail.com>
2020-11-14 15:43:41 -0500

    Added lodash-es

--- babel/utils/transformImports.js
+++ babel/utils/transformImports.js
@@ -20,5 +20,9 @@ module.exports = {
     lodash: {
         transform: 'lodash/${member}',
         preventFullImport: true
+    },
+    'lodash-es': {
+        transform: 'lodash-es/${member}',
+        preventFullImport: true
     }
 };
