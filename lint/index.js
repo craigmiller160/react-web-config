1a796a1ce936ebc2f097ad08185be451660076fe
Craig Miller <craigmiller160@gmail.com>
2021-04-18 11:21:24 -0400

    Setup bare minimum eslint config

---
+++ lint/index.js
@@ -0,0 +1,37 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    env: {
+        browser: true,
+        amd: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended'
+    ],
+    rules: {
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        'no-console': 'error'
+    },
+    overrides: [
+        {
+            files: ['**/*.ts?(x)'],
+            parser: '@typescript-eslint/parser',
+            extends: ['plugin:@typescript-eslint/recommended']
+        }
+    ]
+};
