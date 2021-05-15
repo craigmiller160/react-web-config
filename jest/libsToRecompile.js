8498cf38b900b2ac1285c94fa87816e406ad77d2
Craig <craigmiller160@gmail.com>
2021-05-15 15:51:32 -0400

    Added ajax error handler to jest recompile libs

--- jest/libsToRecompile.js
+++ jest/libsToRecompile.js
@@ -23,5 +23,6 @@ module.exports = [
     '\@craigmiller160\/ajax-api',
     '\@craigmiller160\/jest-matchers-common',
     '\@craigmiller160\/react-protected-route',
-    '\@craigmiller160\/react-hook-form-material-ui'
+    '\@craigmiller160\/react-hook-form-material-ui',
+    '\@craigmiller160\/ajax-error-handler'
 ];
